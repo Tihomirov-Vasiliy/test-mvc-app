2bcd39dbd9d338bf742f32e835d15e361bd68a10
Tihomirov-Vasiliy <tihomirov.vasili@bk.ru>
2023-01-12 23:38:14 +0600

    initial create layers as in clean architecture and define entities

---
+++ src/Domain/Entities/Citizen.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities
+{
+    public class Citizen : BaseEntity
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Gender { get; set; }
+        public string RegistrationAddress { get; set; }
+        public long Inn { get; set; }
+        public long Snils { get; set; }
+    }
+}
---
+++ src/Domain/Entities/Organization.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities
+{
+    public class Organization : BaseEntity
+    {
+        public string FullName { get; set; }
+        public string ShortName { get; set; }
+        public BusinessArea BusinessArea { get; set; }
+        public Citizen Ceo { get; set; }
+        public string AuthorizedCapital { get; set; }
+        public long Inn { get; set; }
+        public long Kpp { get; set; }
+        public long Ogrn { get; set; }
+    }
+}
